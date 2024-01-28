3054282a858c6f43a5f396ceb4f4956bb8010eb3
DarrenLo0530 <darren.lo.0530@gmail.com>
2024-01-28 03:11:01 -0500

    Add sign in sign up buttons

--- frontend/src/pages/JoinRoomPage.tsx
+++ frontend/src/pages/JoinRoomPage.tsx
@@ -1,13 +1,13 @@
 import { Box, Button, Center, Flex, Heading, Input } from "@chakra-ui/react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 interface JoinRoomValues {
   roomId: string;
 }
 
 const JoinRoomPage = () => {
-  const { register, handleSubmit } = useForm<JoinRoomValues>();
+  const { register, handleSubmit, watch } = useForm<JoinRoomValues>();
   const navigate = useNavigate();
 
   const onSubmit: SubmitHandler<JoinRoomValues> = (data) => {
@@ -15,13 +15,22 @@ const JoinRoomPage = () => {
   }
 
   return (
-    <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
-      <Center>
+    
+    <Flex width={"100vw"} height={"100vh"} flexDir={'column'}>
+      <Flex justifyContent={'end'} gap='12px' padding={'16px'} position={'absolute'} width={'100vw'}>
+        <Link to='/sign-in'>
+          <Button>Sign In</Button>
+        </Link>
+        <Link to='/sign-up'>
+          <Button>Sign Up</Button>
+        </Link>
+      </Flex>
+      <Center flex='1'>
         <Box>
           <form onSubmit={handleSubmit(onSubmit)}>
             <Heading as='h2' marginBottom='12px' textAlign='center'>Join A Room</Heading>
             <Input type="text" placeholder="Room Id" marginBottom='12px' {...register('roomId')}></Input>
-            <Button type='submit' width='100%'>Join </Button>
+            <Button type='submit' width='100%' isDisabled={!watch('roomId')}>Join </Button>
           </form>
         </Box>
       </Center>
